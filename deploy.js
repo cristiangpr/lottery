a8795df0e42c5152441ca970ffe22cfa4661483b
Cristian Perez <cristiangp777@gmail.com>
2020-02-06 18:26:44 -0500

    contracts and tests ok

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'paddle confirm spell either movie edit bonus game van hard glass siege',
+  'https://rinkeby.infura.io/v3/4b99b54ed218417aa3371bf079021687'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('attempting to deploy from account', accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({ data: bytecode })
+  .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('contract deployed to', result.options.address);
+
+};
+
+deploy();
---
+++ test/lottery.test.js
@@ -0,0 +1,100 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+// UPDATE THESE TWO LINES RIGHT HERE!!!!! <-----------------
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let lottery;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // Use one of those accounts to deploy the contract
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode})
+    .send({ from: accounts[0], gas: '1000000' });
+
+  // ADD THIS ONE LINE RIGHT HERE!!!!! <---------------------
+  lottery.setProvider(provider);
+});
+
+describe('Lottery contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
+  it('allows one account to enter', async () => {
+     await lottery.methods.enter().send({
+       from: accounts[0],
+       value: web3.utils.toWei('0.02', 'ether')
+     });
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+    assert.equal(1, players.length);
+  });
+  it('allows multiple accounts to enter', async () => {
+     await lottery.methods.enter().send({
+       from: accounts[0],
+       value: web3.utils.toWei('0.02', 'ether')
+     });
+     await lottery.methods.enter().send({
+       from: accounts[1],
+       value: web3.utils.toWei('0.02', 'ether')
+     });
+     await lottery.methods.enter().send({
+       from: accounts[2],
+       value: web3.utils.toWei('0.02', 'ether')
+     });
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+    assert.equal(players[0], accounts[0]);
+    assert.equal(players[1], accounts[1]);
+    assert.equal(players[2], accounts[2]);
+    assert.equal(3, players.length);
+  });
+
+  it('requires a minimum amount of ether is sent to enter the lottery', async () => {
+    try {
+      await lottery.methods.enter().send({ from: accounts[0], value: web3.utils.toWei('0.001', 'ether') });
+    } catch (err) {
+      assert(true);
+      return;
+    }
+    assert(false);
+  });
+
+    it('only managers should pick winner', async () => {
+try {
+      await lottery.methods.pickWinner.send({
+        from: accounts[0]
+      });
+        assert(false);
+    } catch (err) {
+    assert(err);
+
+    }
+
+  });
+  it('sends money to winner and resets players array', async () => {
+         await lottery.methods.enter().send({
+           from: accounts[0],
+           value: web3.utils.toWei('2', 'ether')
+         });
+
+         const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({from: accounts[0]});
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        const difference = finalBalance - initialBalance;
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+        const players = await lottery.methods.getPlayers().call();
+        assert(players.length == 0);
+
+  })
+
+});
